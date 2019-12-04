13aa2630c2bdee5e43a9ef9ae9e5d0dde3ff40dc
asm <asmm@list.ru>
2019-12-05 01:47:08 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,187 @@
+﻿# What is Query2ValidationException for Laravel 5?
+Query2ValidationException throw mysql QueryException which relate to business logic to ValidationException.
+For example:
+500 Column ':attribute' cannot be null
+to
+422 The :attribute field is required.
+
+# Install
+```bash
+composer require asmgit/query2validation-exception
+```
+
+# Quick start
+app/Exception/Handler.php
+```php
+use Asmgit\Validation\Exception;
+...
+public function report(Exception $exception)
+{
+    if (Query2ValidationException::check($exception)) {
+        $this->dontReport[] = QueryException::class;
+        return (new Query2ValidationException($exception))->render();
+    }
+...
+```
+Now you can remove most common Validation's from your code and DB will take care of your data.
+
+# What exctepions catch Query2ValidationException
+After install most common exception will translate to ValidationException:
+* ER_BAD_NULL_ERROR, 1048, (Column '%s' cannot be null) to The :attribute field is required.
+* ER_DUP_ENTRY, 1062, (Duplicate entry '%s' for key %d) to The :attribute has already been taken.
+* ER_DATA_TOO_LONG, 1406, (Data too long for column '%s' at row %ld) to  The :attribute is too long.
+* WARN_DATA_TRUNCATED, 1265, (Data truncated for column '%s' at row %ld) to The selected :attribute is invalid.
+* ER_TRUNCATED_WRONG_VALUE_FOR_FIELD. 1366, (Incorrect %s value: '%s' for column '%s' at row %ld) to The :attribute must be an :field_type type.
+
+You can get all builtin messages
+```php
+dd(Query2ValidationException::getMessageTemplates());
+```
+
+# How set custom message for exception
+```php
+Query2ValidationException::addValidationMessage(
+    'Please, fill your first name.', // new message
+    Query2ValidationException::ER_BAD_NULL_ERROR, // error type
+    'first_name' // field
+);
+$user->save();
+array_pop(Query2ValidationException::$customValidationMessages);
+```
+will return for ajax request
+```json
+{"message":"The given data was invalid."
+,"errors":{"first_name":["Please, fill your first name."]}
+}
+```
+
+# How work composite unique key exception
+Messages will generate for all fields in unique key
+```mysql
+ALTER TABLE users
+ADD UNIQUE INDEX users_account_id_full_name_unique (account_id,full_name)
+```
+```json
+{"message":"The given data was invalid."
+,"errors":{"account_id":["The account_id,full_name has already been taken."]
+    ,"full_name":["The account_id,full_name has already been taken."]
+}}
+```
+
+# How set custom message for composite unique key exception
+## One way:
+Declare COMMENT for unique key
+```mysql
+ALTER TABLE users
+DROP INDEX users_account_id_full_name_unique
+, ADD UNIQUE INDEX users_account_id_full_name_unique (account_id,full_name)
+  COMMENT 'User must be unique for each account. :value has already been taken.'
+```
+```json
+{"message":"The given data was invalid."
+,"errors":{"account_id":["User must be unique for each account. 1-John Smith has already been taken."]
+    ,"full_name":["User must be unique for each account. 1-John Smith has already been taken."]
+}}
+```
+You can use params such :value. See all params:
+```php
+dd(Query2ValidationException::getMessageTemplates());
+```
+
+## Another way (with change field destination):
+```php
+public function store(Request $request)
+{
+    ...
+    Query2ValidationException::addValidationMessage(
+        'User already exists in this account.', // new message
+        Query2ValidationException::ER_DUP_ENTRY, // error type
+        'account_id,full_name', // all fields in composite unique key
+        'first_name,last_name' // new field/fields
+    );
+    $user->save();
+    array_pop(Query2ValidationException::$customValidationMessages);
+...
+public function update($id, Request $request)
+{
+    ...
+    Query2ValidationException::addValidationMessage(
+        'User already exists in this account.', // new message
+        Query2ValidationException::ER_DUP_ENTRY, // error type
+        'account_id,full_name', // all fields in composite unique key
+        'first_name,last_name' // new field/fields
+    );
+    $user->save();
+    array_pop(Query2ValidationException::$customValidationMessages);
+```
+```json
+{"message":"The given data was invalid."
+,"errors":{"first_name":["User already exists in this account."]
+    ,"last_name":["User already exists in this account."]
+}}
+```
+
+# How catch new global mysql exception
+* check error format in mysql doc site:
+https://dev.mysql.com/doc/refman/8.0/en/server-error-reference.html
+* Intitialize builtin messages
+```php
+Query2ValidationException::$messageTemplates = Query2ValidationException::getMessageTemplates();
+```
+* create new message template
+```php
+Query2ValidationException::$messageTemplates[<errorNo>] = ...
+```
+* also you can change builtin messages
+
+Example for mysql >= 8.0.16 with check constraint support:
+app/Exception/Handler.php
+```php
+use Asmgit\Validation\Exception;
+...
+public function report(Exception $exception)
+{
+    // setup builtin messageTemplates
+    Query2ValidationException::$messageTemplates = Query2ValidationException::getMessageTemplates();
+    // Error number: 3819; Symbol: ER_CHECK_CONSTRAINT_VIOLATED; SQLSTATE: HY000 Message: Check constraint '%s' is violated.
+    Query2ValidationException::$messageTemplates[3819] = [
+        'orig' => "Check constraint '(.*?)\\|(.*?)\\|(.*)' is violated.",
+        'new' => "Check constraint is violated: :message",
+        'params' => [1 => 'table_name', 2 => 'attribute', 3 => 'message']
+    ];
+    if (Query2ValidationException::check($exception)) {
+        $this->dontReport[] = QueryException::class;
+        return (new Query2ValidationException($exception))->render();
+    }
+...
+```
+Create and raise any CHECK CONSTRAINT
+```mysql
+ALTER TABLE users
+ADD CONSTRAINT `users|first_name,last_name|first name must be longer than last name.` CHECK (first_name > last_name);
+```
+```json
+{"message":"The given data was invalid."
+,"errors":{"first_name":["Check constraint is violated: first name must be longer than last name."]
+    ,"last_name":["Check constraint is violated: first name must be longer than last name."]
+}}
+```
+
+# What are the disadvantages of Query2ValidationException solution?
+* Mysql raise only one first error.
+
+# What are the advantages of Query2ValidationException solution?
+* You can describe constraints only in UI and DB, against: UI, DB, app->store, app->update rules.
+* Some exception you can't catch in app layer. For example unique constraints.
+This code work fine in oneuser application.
+```php
+public function store(Request $request)
+{
+    $validatedData = $request->validate([
+        'full_name' => 'required|unique:users',
+    ]);
+    sleep(20);
+    $user->full_name = $request->input('full_name');
+    $user->save();
+```
+But if you run this code with new unique full_name twice, one session save data, second get 500 exception (with correct unique constraint DB config, without this you get BSOD ))))
---
+++ src/Query2ValidationException.php
@@ -0,0 +1,210 @@
+<?php
+namespace Asmgit\Validation\Exception;
+
+use Illuminate\Database\QueryException;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Lang;
+use Illuminate\Support\Str;
+use Illuminate\Validation\ValidationException;
+
+class Query2ValidationException extends QueryException
+{
+    // all errors: https://dev.mysql.com/doc/refman/8.0/en/server-error-reference.html
+    const ER_BAD_NULL_ERROR = 1048; // Column '%s' cannot be null
+    const ER_DUP_ENTRY = 1062; // Duplicate entry ‘%s’ for key %d
+    const ER_DATA_TOO_LONG = 1406; // Data too long for column '%s' at row %ld
+    // Enum fields 2 "in" validation
+    const WARN_DATA_TRUNCATED = 1265; // Data truncated for column '%s' at row %ld
+    // Different types
+    const ER_TRUNCATED_WRONG_VALUE_FOR_FIELD = 1366; // Incorrect %s value: '%s' for column '%s' at row %ld
+
+    protected $type;
+
+    protected $paramFieldName = 'attribute';
+
+    protected $fieldName;
+
+    protected $params;
+
+    protected $dbErrorMessage;
+
+    public $errorMessage;
+
+    public static $messageTemplates = null;
+
+    public static $customValidationMessages = null;
+
+    public function __construct($exception)
+    {
+        parent::__construct($exception->getSql(), $exception->getBindings(), $exception);
+        if (self::$customValidationMessages == null) {
+            self::$customValidationMessages = [];
+        }
+        $this->type = $this->errorInfo[1];
+        $this->dbErrorMessage = $this->errorInfo[2];
+        self::$messageTemplates = self::getMessageTemplates();
+    }
+
+    private static function supportedTypes() {
+        $types = [];
+        foreach (self::getMessageTemplates() as $type => $msg) {
+            $types[] = $type;
+        }
+        return $types;
+    }
+
+    public static function check($exception) {
+        return ($exception instanceof QueryException) && in_array($exception->errorInfo[1], self::supportedTypes());
+    }
+
+    public static function getMessageTemplates()
+    {
+        if (self::$messageTemplates != null) return self::$messageTemplates;
+
+        $messageTemplates = [];
+        $messageTemplates[self::ER_BAD_NULL_ERROR] = [
+            'orig' => "Column '(.*?)' cannot be null",
+            'new' => Lang::get('validation.required'),
+            'params' => [1 => 'attribute']
+        ];
+        $messageTemplates[self::ER_DUP_ENTRY] = [
+            'orig' => "Duplicate entry '(.*?)' for key '(.*?)'",
+            'new' => Lang::get('validation.unique'),
+            'params' => [1 => 'value', 2 => 'index_name'],
+            'params_post_process' => function(&$ex) {
+                // get field name from unique constraint key name. Example: users_email_unique
+                try {
+                    $q = DB::select("SELECT GROUP_CONCAT(S.COLUMN_NAME ORDER BY S.SEQ_IN_INDEX) col_names
+                    , MAX(S.TABLE_NAME) table_name
+                    , MAX(S.INDEX_COMMENT) index_comment
+                    FROM INFORMATION_SCHEMA.STATISTICS S
+                    WHERE S.TABLE_SCHEMA = DATABASE()
+                     AND S.INDEX_NAME = :index_name",
+                        ['index_name' => $ex->params['index_name']]
+                    )[0];
+                } catch (\Exception $e) {
+                    throw new \Exception($e);
+                }
+                $ex->params['attribute'] = $q->col_names;
+                $ex->params['table_name'] = $q->table_name;
+                $ex->errorMessage = $q->index_comment ?: $ex->errorMessage;
+            }
+        ];
+        $messageTemplates[self::ER_DATA_TOO_LONG] = [
+            'orig' => "Data too long for column '(.*?)' at row ([0-9]+)",
+            'new' => 'The :attribute is too long.',
+            'params' => [1 => 'attribute', 2 => 'rownum']
+        ];
+        $messageTemplates[self::WARN_DATA_TRUNCATED] = [
+            'orig' => "Data truncated for column '(.*?)' at row ([0-9]+)",
+            'new' => Lang::get('validation.in'),
+            'params' => [1 => 'attribute', 2 => 'rownum']
+        ];
+        $messageTemplates[self::ER_TRUNCATED_WRONG_VALUE_FOR_FIELD] = [
+            'orig' => "Incorrect (.*?) value: '(.*?)' for column '(.*?)' at row ([0-9]+)",
+            'new' => 'The :attribute must be an :field_type type.',
+            'params' => [1 => 'field_type', 2 => 'value', 3 => 'attribute', 4 => 'rownum']
+        ];
+        return $messageTemplates;
+    }
+
+    private function processMessage()
+    {
+        $msg = self::$messageTemplates[$this->type];
+        $this->errorMessage = $msg['new'];
+        $this->paramFieldName = $msg['field'] ?? $this->paramFieldName;
+        preg_match('~' . $msg['orig'] . '~', $this->dbErrorMessage, $matches);
+        foreach ($msg['params'] as $matchIndex => $paramName) {
+            $this->params[$paramName] = $matches[$matchIndex];
+        }
+        if (isset($msg['params_post_process'])) {
+            $msg['params_post_process']($this);
+        }
+        $this->fieldName = $this->params[$this->paramFieldName];
+
+        foreach (self::$customValidationMessages as $validation) {
+            if ($validation['errorType'] == $this->type
+                && ($validation['field'] == $this->fieldName || $validation['field'] == null)
+            ) {
+                $this->fieldName = $validation['newField'] ?? $this->fieldName;
+                $this->errorMessage = $validation['message'];
+                break;
+            }
+        }
+
+        $this->errorMessage = $this->makeReplacements($this->errorMessage, $this->params);
+    }
+
+    /**
+     * Render an exception into an HTTP response.
+     */
+    public function render()
+    {
+        $this->processMessage();
+        // explode fields name by ,
+        $msg = [];
+        $fields = explode(',', $this->fieldName);
+        foreach ($fields as $field) {
+            $msg[$field] = [$this->errorMessage];
+        }
+        throw ValidationException::withMessages($msg);
+    }
+
+    public static function addValidationMessage($message, $errorType, $field = null, $newField = null)
+    {
+        if (self::$customValidationMessages == null) {
+            self::$customValidationMessages = [];
+        }
+        self::$customValidationMessages[] = ['message' => $message,
+            'errorType' => $errorType,
+            'field' => $field,
+            'newField' => $newField
+        ];
+    }
+
+    /**
+     * copy paste
+     * Illuminate\Translation\Translator->makeReplacements
+     *
+     * Make the place-holder replacements on a line.
+     *
+     * @param  string  $line
+     * @param  array   $replace
+     * @return string
+     */
+    protected function makeReplacements($line, array $replace)
+    {
+        if (empty($replace)) {
+            return $line;
+        }
+
+        $replace = $this->sortReplacements($replace);
+
+        foreach ($replace as $key => $value) {
+            $line = str_replace(
+                [':'.$key, ':'.Str::upper($key), ':'.Str::ucfirst($key)],
+                [$value, Str::upper($value), Str::ucfirst($value)],
+                $line
+            );
+        }
+
+        return $line;
+    }
+
+    /**
+     * copy paste
+     * Illuminate\Translation\Translator->sortReplacements
+     *
+     * Sort the replacements array.
+     *
+     * @param  array  $replace
+     * @return array
+     */
+    protected function sortReplacements(array $replace)
+    {
+        return (new Collection($replace))->sortBy(function ($value, $key) {
+            return mb_strlen($key) * -1;
+        })->all();
+    }
+}
